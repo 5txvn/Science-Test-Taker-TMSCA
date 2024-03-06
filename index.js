84c33b9bcde4e13181f3a987f4c12500bd10faf1
Steven Livingston <therealenny1@gmail.com>
2024-03-05 22:07:30 -0600

    Add response time function and add option for no code available

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//import all the required libs
 const prompt = require('prompt-sync')();
 const chalk = require('chalk');
 const fs = require('fs');
@@ -13,17 +14,21 @@ if (hasCode == "y") {
     } else {
         key = JSON.parse(fs.readFileSync(`./keys/${temp[0]}/${temp[1]}.json`, "utf8"))
     }
+} else if (hasCode == "n") {
+    const code = prompt("Please enter the JSON for the key: ")
+    key = JSON.parse(code)
 }
 //gpt prompt for key generation - Convert the following into json with the keys as the question numbers (ie. "1" or "17) and the values as the corresponding answers in capital letters
 while(questionNumber<=20) {
+    let currentTime = Date.now();
     let question = prompt(questionNumber + ": ");
     if (question == key[questionNumber]) {
-        console.log(chalk.green("Correct :)"))
+        console.log(chalk.green(`Correct! Response time was ${(Date.now() - currentTime) / 1000} seconds`))
         questionNumber++
     } else if (question == "quit") {
         break;
     } else {
-        console.log(chalk.red(`Incorrect, correct answer is ${chalk.bold(key[questionNumber])}`))
+        console.log(chalk.red(`Incorrect, correct answer is ${chalk.bold(key[questionNumber])}, and response time was ${(Date.now() - currentTime) / 1000} seconds`))
         questionNumber++
     }
 }
